bcc81ab65d76d914589a06327635519f54b47d2d
Yaoobs <yaozheng1990517@163.com>
2018-01-10 13:57:43 +0800

    add ReactNative Demo SampleAppMovies

---
+++ ReactNative/SampleAppMovies/App.js
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {
+  Component,
+} from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+var MOCKED_MOVIES_DATA = [
+  {title: '标题', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+export default class App extends Component<{}> {
+ render() {
+    var movie = MOCKED_MOVIES_DATA[0];
+    return (
+      <View style={styles.container}>
+        <Text>{movie.title}</Text>
+        <Text>{movie.year}</Text>
+        <Image source={{uri: movie.posters.thumbnail}}
+               style={styles.thumbnail}
+         />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+   thumbnail: {
+    width: 53,
+    height: 81,
+  },
+});
+
