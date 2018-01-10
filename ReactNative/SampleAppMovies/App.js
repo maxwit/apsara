78853aef6384684bdad9366fe31fb285ca291630
Yaoobs <yaozheng1990517@163.com>
2018-01-10 17:09:29 +0800

    fetchData

--- ReactNative/SampleAppMovies/App.js
+++ ReactNative/SampleAppMovies/App.js
@@ -18,19 +18,70 @@ var MOCKED_MOVIES_DATA = [
   {title: '标题', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
 
+/**
+ * 为了避免骚扰，我们用了一个样例数据来替代Rotten Tomatoes的API
+ * 请求，这个样例数据放在React Native的Github库中。
+ * 当然，由于众所周知的原因，这个地址可能国内访问也比较困难。
+ */
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json';
+
 export default class App extends Component<{}> {
- render() {
-    var movie = MOCKED_MOVIES_DATA[0];
+
+ constructor(props) {
+    super(props);   //这一句不能省略，照抄即可
+    this.state = {
+      movies: null,  //这里放你自己定义的state变量及初始值
+    };
+    // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向不对
+    // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+   componentDidMount() {
+    this.fetchData();
+  }
+
+   fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
+        this.setState({
+          movies: responseData.movies,
+        });
+      });
+  }
+
+render() {
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Image 
+        <Text>
+          正在加载电影数据……
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
           source={{uri: movie.posters.thumbnail}}
           style={styles.thumbnail}
-         />
-          <View style={styles.rightContainer}>
-            <Text style={styles.title}>{movie.title}</Text>
-            <Text style={styles.year}>{movie.year}</Text>
-          </View>
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
