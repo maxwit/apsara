43329e02de10f20b288da75baf44722b0d3ab6bc
Yaoobs <yaozheng1990517@163.com>
2018-01-10 14:25:28 +0800

    Flex Layout

--- ReactNative/SampleAppMovies/App.js
+++ ReactNative/SampleAppMovies/App.js
@@ -23,11 +23,14 @@ export default class App extends Component<{}> {
     var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
-        <Image source={{uri: movie.posters.thumbnail}}
-               style={styles.thumbnail}
+        <Image 
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
          />
+          <View style={styles.rightContainer}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
+          </View>
       </View>
     );
   }
@@ -36,9 +39,21 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+   rightContainer: {
+    flex: 1,
+  },
+   title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
   },
    thumbnail: {
     width: 53,
